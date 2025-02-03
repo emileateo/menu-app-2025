afb957c51840a138404f1e4bec0899292bebeda8
Emilea Teo <emileateo@gmail.com>
2025-02-04 01:16:29 +0800

    troubleshooting: some console.logs

--- menu_frontend/src/App.tsx
+++ menu_frontend/src/App.tsx
@@ -36,6 +36,8 @@ const GET_SECTIONS = gql`
 
 function App() {
   const { loading, error, data } = useQuery(GET_SECTIONS);
+
+  console.log('data', data);
   
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -47,34 +49,36 @@ function App() {
     })
   ) || [];
   
+  console.log('sections', sections);
+
   const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
   const [activeSection, setActiveSection] = useState(sections[0]?.id || "");
 
-  useEffect(() => {
-    if (Object.keys(sectionRefs.current).length === 0) return;
+  // useEffect(() => {
+  //   if (Object.keys(sectionRefs.current).length === 0) return;
   
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setActiveSection(Number(entry.target.id));
-            entry.target.classList.add('active'); 
-          } else {
-            entry.target.classList.remove('active'); 
-          }
-        });
-      },
-      { rootMargin: "0px 0px -99% 0px", threshold: 0 }
-    );
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           setActiveSection(Number(entry.target.id));
+  //           entry.target.classList.add('active'); 
+  //         } else {
+  //           entry.target.classList.remove('active'); 
+  //         }
+  //       });
+  //     },
+  //     { rootMargin: "0px 0px -99% 0px", threshold: 0 }
+  //   );
   
-    Object.values(sectionRefs.current).forEach((section) => {
-      if (section) {
-        observer.observe(section);
-      }
-    });
+  //   Object.values(sectionRefs.current).forEach((section) => {
+  //     if (section) {
+  //       observer.observe(section);
+  //     }
+  //   });
   
-    return () => observer.disconnect();
-  }, [sections]);
+  //   return () => observer.disconnect();
+  // }, [sections]);
 
   const handleSectionClick = (sectionId: string) => {
     const sectionElement = sectionRefs.current[sectionId];
